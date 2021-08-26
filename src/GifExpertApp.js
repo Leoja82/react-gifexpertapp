5c9ea0db85fe9014819428d916c9c819d12d03cc
Leonardo Avalos <avalosleo@gmail.com>
2021-08-26 10:44:16 -0300

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = () => {
+
+   
+    const [categories, setCategories] = useState(['futurama']);
+
+    //const handleAdd = () => {
+      //  setCategories( [...categories,{value}] );
+      //  setCategories( cats => [...cats,'Futurama']
+
+    // }
+    
+    return(
+         <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+            
+        <ol>
+            {
+                categories.map( (category) => ( 
+                    <GifGrid 
+                        key={category}
+                        category={category} />
+                ))
+            }
+        </ol>
+
+        </>
+    )
+}
+
+
+export default GifExpertApp;
